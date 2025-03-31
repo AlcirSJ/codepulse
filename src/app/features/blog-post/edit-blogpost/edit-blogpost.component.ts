08f1c67bba9e5370828506542a8a74193479e8a6
esoft02 <ex188928@g.unicamp.br>
2025-03-31 14:40:20 -0300

    delete blog button

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -12,104 +12,113 @@ import { UpdateBlogPost } from '../models/update-blog-post-model';
 @Component({
   selector: 'app-edit-blogpost',
   templateUrl: './edit-blogpost.component.html',
-  styleUrls: ['./edit-blogpost.component.css']
+  styleUrls: ['./edit-blogpost.component.css'],
 })
 export class EditBlogpostComponent implements OnInit, OnDestroy {
-
   id: string | null = null;
+
   routeSubscription?: Subscription;
   updateBlogPostSubscription?: Subscription;
   getBlogPostSubscription?: Subscription;
-  model?: BlogPost
+  deleteBlogPostSubscription?: Subscription;
+
+  model?: BlogPost;
   categories$?: Observable<Category[]>;
   selectedCategories?: string[];
   categoryDropdown: Category[] = [];
-  dropdownSettings: IDropdownSettings = {singleSelection: false,
-                                          idField: 'id',
-                                          textField: 'name',   
-                                          itemsShowLimit: 5,
-                                          allowSearchFilter: true,
-                                          defaultOpen: false};
+  dropdownSettings: IDropdownSettings = {
+    singleSelection: false,
+    idField: 'id',
+    textField: 'name',
+    itemsShowLimit: 5,
+    allowSearchFilter: true,
+    defaultOpen: false,
+  };
 
- 
-
-  constructor(private route: ActivatedRoute,  
-              private blogPostService: BlogPostService,
-              private categoriesService: CategoryService,
-              private toastr: ToastrService,
-              private router: Router
-  ) { }
-
+  constructor(
+    private route: ActivatedRoute,
+    private blogPostService: BlogPostService,
+    private categoriesService: CategoryService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-
-   this.categories$ =  this.categoriesService.getAllCategories();
+    this.categories$ = this.categoriesService.getAllCategories();
 
     this.routeSubscription = this.route.paramMap.subscribe({
-      next: params => {
-       this.id = params.get('id')
+      next: (params) => {
+        this.id = params.get('id');
 
-       if(this.id){      
-        this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
-          next: (response) => {
-            this.model = response; 
-            this.selectedCategories = response.categories.map(x => x.id);                     
-          }
-       });
+        if (this.id) {
+          this.getBlogPostSubscription = this.blogPostService
+            .getBlogPostById(this.id)
+            .subscribe({
+              next: (response) => {
+                this.model = response;
+                this.selectedCategories = response.categories.map((x) => x.id);
+              },
+            });
+        }
+      },
+    });
 
-      }      
-     }
-   });
-   
-   this.dropDownSettings();  
+    this.dropDownSettings();
   }
-
 
   onFormSubmit(): void {
-    if(this.model && this.id){
-    var updateBlogPost: UpdateBlogPost = {
-          author: this.model.author,
-          content: this.model.content,
-          shortDescription: this.model.shortDescription,
-          featuredImageUrl: this.model.featuredImageUrl,
-          isVisible: this.model.isVisible,  
-          publishedDate: this.model.publishedDate,
-          title: this.model.title,
-          urlHandle: this.model.urlHandle,
-          categories: this.selectedCategories ?? []    
-      }; 
-      
-      this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
-      .subscribe({
-      next: (response) =>{
-        this.router.navigate(['/admin/blogposts']);
-      }
-      });
+    if (this.model && this.id) {
+      var updateBlogPost: UpdateBlogPost = {
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? [],
+      };
 
-
+      this.updateBlogPostSubscription = this.blogPostService
+        .updateBlogPost(this.id, updateBlogPost)
+        .subscribe({
+          next: (response) => {
+            this.router.navigate(['/admin/blogposts']);
+          },
+        });
     }
-    
-    
-}
-
-dropDownSettings(){  
-  this.categoriesService.getAllCategories()  
-    .subscribe({      
-      next: (data) => {
-        this.categoryDropdown = data;
-       
-      },
-      error: () => {
-        this.toastr.error('Error loading categories','Error', {timeOut: 15000});
-      }
-    });
-}
-
-
-  ngOnDestroy(): void {
-    this.routeSubscription?.unsubscribe(); 
-    this.updateBlogPostSubscription?.unsubscribe();
-    this.getBlogPostSubscription?.unsubscribe();
   }
 
+  dropDownSettings() {
+    this.categoriesService.getAllCategories().subscribe({
+      next: (data) => {
+        this.categoryDropdown = data;
+      },
+      error: () => {
+        this.toastr.error('Error loading categories', 'Error', {
+          timeOut: 15000,
+        });
+      },
+    });
+  }
+
+  onDelete(): void {
+    if (this.id) {
+      this.deleteBlogPostSubscription = this.blogPostService
+        .deleteBlogPost(this.id)
+        .subscribe({
+          next: (response) => {
+            this.router.navigateByUrl('/admin/blogposts');
+          },
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
+    this.getBlogPostSubscription?.unsubscribe();
+    this.deleteBlogPostSubscription?.unsubscribe();
+  }
 }
